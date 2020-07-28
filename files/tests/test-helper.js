074887673b4ac9c83e751b3d066a08700bf90be0
Bill Dami <billdami@gmail.com>
2020-07-28 13:56:46 -0400

    missed modulePrefix placeholder

--- files/tests/test-helper.js
+++ files/tests/test-helper.js
@@ -1,4 +1,4 @@
-import Application from 'project-setup-test/app';
+import Application from '<%= modulePrefix %>/app';
 import config from '<%= modulePrefix %>/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
