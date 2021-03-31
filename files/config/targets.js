3b715d2d47de68d73346c046ac470e6a421d77c6
Nick Pafundi <npafundi@gavant.com>
2021-03-31 09:54:51 -0400

    updated blueprint

--- files/config/targets.js
+++ files/config/targets.js
@@ -1,10 +1,6 @@
 'use strict';
 
-const browsers = [
-    'last 1 Chrome versions',
-    'last 1 Firefox versions',
-    'last 1 Safari versions'
-];
+const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
 
 const isCI = Boolean(process.env.CI);
 const isProduction = process.env.EMBER_ENV === 'production';
@@ -14,5 +10,10 @@ if (isCI || isProduction) {
 }
 
 module.exports = {
-    browsers
+    browsers,
+    // This fixes some issue where the app breaks in fastboot.
+    // https://discord.com/channels/480462759797063690/485861149821239298/818887228011839499
+    // Additionally there's a PR that supposedly fixed it for Ember 3.25+ but it still
+    // doesn't work without this: https://github.com/emberjs/ember.js/pull/19397
+    node: 'current'
 };
--- files/types/ember-cli-notifications/services/notifications.d.ts
+++ files/types/ember-cli-notifications/services/notifications.d.ts
@@ -1,14 +1,17 @@
 import Service from '@ember/service';
 
-declare interface NotificationOptions {
+export interface NotificationOptions {
     message: string;
-    type: string;
+    type?: string;
     autoClear?: boolean;
     clearDuration?: number;
     onClick?(): any;
     htmlContent?: boolean;
     cssClasses?: string[];
+    remaining?: number;
 }
+
+export interface Notification extends NotificationOptions {}
 
 declare module 'ember-cli-notifications/services/notifications' {
     export default class NotificationService extends Service {
@@ -17,5 +20,11 @@ declare module 'ember-cli-notifications/services/notifications' {
         error(message: string, options?: NotificationOptions): Object;
         info(message: string, options?: NotificationOptions): Object;
         warning(message: string, options?: NotificationOptions): Object;
+        removeNotification(notification: Notification): void;
+        setupAutoClear(notification: Notification): void;
+        pauseAutoClear(notification: Notification): void;
+        clearAll(): NotificationService;
+        setDefaultAutoClear(autoClear: boolean): void;
+        setDefaultClearDuration(clearDuration: number): void;
     }
 }
