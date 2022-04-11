ab4f89cee0327829e616fe947bd29b1be7be7751
Adam Baker <bakerac4@gmail.com>
2022-04-11 12:24:50 -0400

    Fixing test

--- files/tests/unit/services/current-user-test.ts
+++ files/tests/unit/services/current-user-test.ts
@@ -4,11 +4,17 @@ import { setupTest } from 'ember-qunit';
 import CurrentUserService from '<%= modulePrefix %>/services/current-user';
 import { module, test } from 'qunit';
 
+import type { TestContext } from 'ember-test-helpers';
+
+interface Context extends TestContext {
+    owner: any;
+}
+
 module('Unit | Service | current-user', function (hooks) {
     setupTest(hooks);
     setupMirage(hooks);
 
-    test('Loading the current user works', async function (assert) {
+    test('Loading the current user works', async function (this: Context, assert) {
         this.server.create('user', { id: '1', firstName: 'Emma', lastName: 'Baker' });
         const service = this.owner.lookup('service:current-user') as CurrentUserService;
         await service.load();
