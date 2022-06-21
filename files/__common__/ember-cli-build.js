0537394601b13d40249f6ff88eb684e6d1c1816c
Bill Dami <billdami@gmail.com>
2022-06-21 11:38:47 -0400

    global blueprint updates

--- files/__common__/ember-cli-build.js
+++ files/__common__/ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
     let app = new EmberApp(defaults, {
         autoImport: {
-            publicAssetURL: process.env.ASSETS_BASE_URL
+            publicAssetURL: `${process.env.ASSETS_BASE_URL}assets/`
         },
         fingerprint: {
             prepend: process.env.ASSETS_BASE_URL
